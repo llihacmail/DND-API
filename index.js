0cdc53f0359c172b6bcb7cb3d6989e25fb9d9e4d
Liam <liamecahill@gmail.com>
2023-03-16 11:35:12 -0400

    added base functions and routes, YML base configuration done with current Users table in AWS. sls invoke local on getUser_query not working currently

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
+module.exports = {
+  getUser: require('./src/api/getUser'),
+  getUsers: require('./src/api/getUsers'),
+  updateUser: require('./src/api/updateUser'),
+  addUser: require('./src/api/addUser'),
+  deleteUser: require('./src/api/deleteUser')
+}
---
+++ src/api/getUser.js
@@ -0,0 +1,11 @@
+const getUser_query = require('../port/getUser_query')
+
+ const getUser = async (event) => {
+    const user = await getUser_query("1")
+    return {
+        statusCode: 200,
+        body: JSON.stringify(user)
+    }
+};
+
+module.exports = getUser
