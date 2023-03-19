2fdf6feb27763f5a50c68c854097ef5de65d5936
Liam <liamecahill@gmail.com>
2023-03-19 19:19:38 -0400

    @getUser_query.js finished refactoring / FIXED * working *

--- src/port/getUser_query.js
+++ src/port/getUser_query.js
@@ -2,19 +2,25 @@ const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient({region: "us-east-1"});
 const { USERS_TABLE } = process.env //to import from .yml
 
-async function getUser_query(userID) {
-    const query = {
-        TableName: process.env.USERS_TABLE,
-        KeyConditionExpression: "#uId = :uId",
-        ExpressionAttributeNames: {
-            "#uId": "UserID"
-        },
-        ExpressionAttributeValues: {
-            ':uId': userID
+async function getUser_query(user) {
+    try {
+        const query = {
+            TableName: process.env.USERS_TABLE,
+            KeyConditionExpression: "#uId = :uId",
+            ExpressionAttributeNames: {
+                "#uId": "UserID"
+            },
+            ExpressionAttributeValues: {
+                ':uId': user.UserID
+            }
         }
+        const result = await dynamoDb.query(query).promise();
+    
+        return result && result.Items.length && result.Items || "NOT_FOUND" //NULL CHECK THE DB OBJECT
     }
-    const result = await dynamoDb.query(query).promise();
+    catch (err) {
+        throw err
+    }
 
-    return result && result.Items.length && result.Items || "NOT_FOUND" //NULL CHECK THE DB OBJECT
 }
 module.exports = getUser_query
