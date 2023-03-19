5d2d3806798b66c28797f5a516b5e977efc78aa3
Liam <liamecahill@gmail.com>
2023-03-19 19:27:43 -0400

    @getUsers_scan.js REFACTORED - added error handling / debugging logs - return NULL check still working * currently working as intended *

--- src/port/getUsers_scan.js
+++ src/port/getUsers_scan.js
@@ -3,10 +3,15 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient({region: "us-east-1"});
 const { USERS_TABLE } = process.env //to import from .yml
 
 async function getUsers_scan() {
-    const result = await dynamoDb.scan(
-        { TableName: process.env.USERS_TABLE }).promise();
-
-    return result.Items|| "NOT_FOUND" //NULL CHECK THE DB OBJECT
+    try {
+        const result = await dynamoDb.scan(
+            { TableName: process.env.USERS_TABLE }).promise();
+    
+        return result.Items|| "NOT_FOUND" //NULL CHECK THE DB OBJECT
+    }
+    catch (err) {
+        throw err
+    }
 
 }
 module.exports = getUsers_scan;
