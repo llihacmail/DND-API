84dfb17cdcaebf0eef673612a18d0016f103b802
Liam <liamecahill@gmail.com>
2023-03-19 19:25:06 -0400

    @getUsers.js finished refactoring / fixed hard coded func data (replaced with event.body.json) - error handling / debugging added * currently working as intended *

--- src/api/getUsers.js
+++ src/api/getUsers.js
@@ -1,11 +1,19 @@
 const getUsers_scan = require('../port/getUsers_scan.js'); 
 
-const getUsers = async (event) => {
-    const users = await getUsers_scan();
-    return {
-        statusCode: 200,
-        body: JSON.stringify(users)
+const getUsers = async () => {
+    try {
+        const users = await getUsers_scan();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(users)
+        }
     }
-};
-
+    catch (err) {
+        console.log("ERROR_SCANNING_DB", err)
+        return {
+            statusCode: 500,
+            body: JSON.stringify("FAILED_TO_FETCH_USERS")
+        }
+    }
+}
 module.exports = getUsers;
