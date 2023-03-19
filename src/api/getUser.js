45972aa8c489f351b63d6fac43ec6b013157f6f1
Liam <liamecahill@gmail.com>
2023-03-19 19:10:54 -0400

    finished functionality of getUser / refactoring of previously working functions - added error handling/debugging logs - @getUser_query.js corrected user event property passed into function as arg. - @getUser_query.js fixed ExpressionAttributesValues to access the user objects properties * currently working as intended *

--- src/api/getUser.js
+++ src/api/getUser.js
@@ -1,11 +1,21 @@
 const getUser_query = require('../port/getUser_query')
 
  const getUser = async (event) => {
-    const user = await getUser_query("1")
-    return {
-        statusCode: 200,
-        body: JSON.stringify(user)
+    const payload = event.body
+    try {
+        const user = await getUser_query(payload)
+        return {
+            statusCode: 200,
+            body: JSON.stringify(user)
+        }
     }
-};
+    catch (err) {
+        console.log(`error fetching ${payload}`, err)
+        return {
+            statusCode: 500,
+            body: JSON.stringify("ERROR_FETCHING_USER")
+        }
+    }
+ }
 
 module.exports = getUser;
